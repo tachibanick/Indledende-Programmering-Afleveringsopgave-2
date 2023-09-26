7fa0ff220d92b5c29a6b78ec4e72a5291293ee84
Nicholas Lentfer Kristiansen <146109232+tachibanick@users.noreply.github.com>
2023-09-26 18:09:45 +0200

    Third time i changed something

--- Palindrome.java
+++ Palindrome.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         // Initialize Scanner and is called to define input. And also hi and also hi
-        // again
+        // again and also hi again again
         Scanner console = new Scanner(System.in);
 
         String input = getWord(console,
