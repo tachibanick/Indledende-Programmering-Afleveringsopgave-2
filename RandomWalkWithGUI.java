d21e923c8eeb5bfb0b41dc1c5335e464988cd337
GustavPetersen <146115993+GustavPetersen@users.noreply.github.com>
2023-09-26 18:18:20 +0200

    Add files via upload

--- RandomWalkWithGUI.java
+++ RandomWalkWithGUI.java
@@ -1,9 +1,10 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class RandomWalk {
+public class RandomWalkWithGUI {
     public static void main(String[] args) {
         int gridSize = getInt(new Scanner(System.in), "Enter size of grid: ");
+        drawGUI(gridSize);
         doPath(gridSize);
     }
 
@@ -18,6 +19,7 @@ public class RandomWalk {
     }
 
     public static void doPath(int gridSize) {
+        StdDraw.setPenColor(StdDraw.BLUE);
         int x = 0, y = 0;
         int steps = 0;
         do {
@@ -28,12 +30,27 @@ public class RandomWalk {
                 y = doRandomMove(y);
             }
             steps++;
+            StdDraw.point(x + gridSize * 0.05, y + gridSize * 0.05);
         } while ((x >= -gridSize && x <= gridSize) && (y >= -gridSize && y <= gridSize));
         System.out.println("ILLEGAL Position = (" + x + "," + y + ")");
         System.out.println("Total number of steps = " + steps);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.point(x + gridSize * 0.05, y + gridSize * 0.05);
     }
 
     public static int doRandomMove(int position) {
         return new Random().nextInt(2) == 1 ? position + 1 : position - 1;
+    }
+
+    public static void drawGUI(int size) {
+        StdDraw.setXscale(-size, size + 1);
+        StdDraw.setYscale(-size, size + 1);
+        StdDraw.setPenRadius(0.01); // Test 0.01 & 0.03 & 0.06 & 0.09
+        for (int i = -size; i <= size; i++) {
+            for (int j = -size; j <= size; j++) {
+                StdDraw.point(i + size * 0.05, j + size * 0.05);
+            }
+        }
+
     }
 }
