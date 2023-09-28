376ca5231da3307223b8de5643a6b3ce05c051c1
Gustav Walker Petersen <146115993+GustavPetersen@users.noreply.github.com>
2023-09-28 17:48:19 +0200

    Boys, jeg har lavet comments!

--- RandomWalk.java
+++ RandomWalk.java
@@ -1,24 +1,40 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/* 
+ * Gustav Walker Petersen.
+ * 02100 Indledende Programmering E23.
+ * This program simulates a random walk in a n-sized 2D grid.
+ */
+
 public class RandomWalk {
     public static void main(String[] args) {
-        int gridSize = getInt(new Scanner(System.in), "Enter size of grid: ");
+        int gridSize = getPosInt(new Scanner(System.in), "Enter size of grid: ");
         doPath(gridSize);
     }
 
+    // This method reads input from the console.
+    // Then it evaluates whether the input is an positive Integer or not.
+    // The method returns the input if it is, else an error will be thrown.
     public static int getPosInt(Scanner console, String prompt) {
-        System.out.println(prompt);
+        System.out.print(prompt);
         if (!console.hasNextInt()) {
             throw new IllegalArgumentException("Please enter an integer");
         }
         int temp = console.nextInt();
-        if (temp < 0){
+        if (temp < 0) {
             throw new IllegalArgumentException("Please enter a positive integer");
-        }       
+        }
         return temp;
     }
 
+    // This method is used to generate the path our entity makes.
+    // The method takes the grid size as an integer then it
+    // prints the entity position to the console and makes
+    // the entity take a random step until the entity is
+    // outside the grid.
+    // Then it prints the illegal position and the total number of steps to the
+    // console.
     public static void doPath(int gridSize) {
         int x = 0, y = 0;
         int steps = 0;
--- RandomWalkGUI.java
+++ RandomWalkGUI.java
@@ -1,6 +1,13 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/* 
+ * Gustav Walker Petersen.
+ * 02100 Indledende Programmering E23.
+ * This program simulates a random walk in a n-sized 2D grid.
+ * And displays it using a GUI. 
+ */
+
 public class RandomWalkGUI {
     public static void main(String[] args) {
         int gridSize = getPosInt(new Scanner(System.in), "Enter size of grid: ");
@@ -8,18 +15,27 @@ public class RandomWalkGUI {
         doPath(gridSize);
     }
 
+    // This method reads input from the console.
+    // Then it evaluates whether the input is an positive Integer or not.
+    // The method returns the input if it is, else an error will be thrown.
     public static int getPosInt(Scanner console, String prompt) {
-        System.out.println(prompt);
+        System.out.print(prompt);
         if (!console.hasNextInt()) {
             throw new IllegalArgumentException("Please enter an integer");
         }
         int temp = console.nextInt();
-        if (temp < 0){
+        if (temp < 0) {
             throw new IllegalArgumentException("Please enter a positive integer");
-        }       
+        }
         return temp;
     }
 
+    // This method is used to generate the path our entity makes.
+    // The method takes the grid size as an integer then it
+    // prints the entity position to the console, draws it to the
+    // GUI and makes the entity take a random step until the entity
+    // is outside the grid. Then it prints the illegal position and
+    // the total number of steps to the console.
     public static void doPath(int gridSize) {
         int x = 0, y = 0;
         int steps = 0;
@@ -38,6 +54,8 @@ public class RandomWalkGUI {
         System.out.println("Total number of steps = " + steps);
     }
 
+    // This method takes the grid size as an argument
+    // and defines the borders of the grid and the point size.
     public static void drawGUI(int size) {
         StdDraw.setXscale(-size, size);
         StdDraw.setYscale(-size, size);
